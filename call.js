c57e68e2693c887e7338f80769c2f70894514081
msanders <msanders45@gmail.com>
2013-08-30 09:08:00 -0600

    initial commit

---
+++ call.js
@@ -0,0 +1,149 @@
+
+$(document).ready(function(){
+    _self = this;
+    var globData = {};
+    $('#sm').click(function(){
+	window.location.href = "http://184.22.119.250/index.html";
+    });
+    $('#home').click(function(){
+	window.location.href = "http://184.22.119.250/index.html";
+    });
+    $('#getSites').click(function(){
+	testDB('getSites',null);
+    });
+    $('#getCustomers').click(function(){
+	testDB('getCustomers',null);
+    });
+    $('#showAddSite').click(function(){
+       $('#myModalLabel').html('Add Site');
+	$('#editSite').hide();
+	$('#addSite').show();
+	$('#myModal').show();
+    });
+    $('#addSite').click(function(){
+	var data = {name:$('#siteName').val(),url:$('#siteUrl').val()};
+       testDB('addSite',data);
+
+    });
+    $('#delSite').click(function(){
+	var data = {id:$('#delSite').data('val')};
+       testDB('delSite',data);
+
+    });
+
+ //handle edit click
+    $("#editSite").click(function(e){
+	e.preventDefault();
+	var val = $('#formID').val();
+	var name = $('#siteName').val();
+	var url = $('#siteUrl').val();
+	var data = {id:val,url:url,name:name};
+	testDB('updateSite',data);
+
+    });
+});
+
+
+function testDB(query,data){
+
+$.ajax({
+	url: "dbconn.php",
+    	type:"POST",
+    	data:{query:query,data:data},
+    	dataType:"json",
+    	success: function(results){
+		handleResults(results);
+    	},
+	error: function(error){
+		console.log('error with server' + error);
+	}
+	});
+}
+
+function handleResults(results){
+	switch(results.type){
+		case 'getSites' :
+			createDataGrid(results.results);
+		break;
+		case 'getCustomers' :
+			createDataGrid(results.results);
+
+		break;
+		case 'delSite' :
+			$('#deleteModal').modal('hide');
+			testDB('getSites',null);
+		break;
+		case 'addSite' :
+			$('#myModal').modal('hide');
+			testDB('getSites',null);
+		break;
+		case 'addCustomer' :
+		break;
+		case 'getCustomersBySite' :
+			createDataGrid(results);
+		break;
+		case 'updateSite':
+			$('#myModal').modal('hide');
+			testDB('getSites',null);
+		break;
+	}
+
+}
+
+
+function createDataGrid(data){
+	$('#dataGrid').html('');
+	//create header for the DataGrid
+	
+	$('#dataGrid').append("<thead>")
+	for(var item in data[0]){
+		$('#dataGrid').append("<th>"+item.toUpperCase()+"</th>");
+	}
+	$('#dataGrid').append("<th></th>");
+
+	$('#dataGrid').append("</thead>")
+	
+	//create rows for DataGrid
+	for(var row in data){
+		$('#dataGrid').append("<tr id="+row+">");
+		for(var item in data[row]){
+			$('#'+row).append("<td>"+data[row][item]+"</td>");
+		}
+		$('#'+row).append("<a href='#' name='addCust' data-val='"+data[row]['id']+"' title='addCust'><i class='icon-plus icon-gray'></i></a>&nbsp;");
+
+		$('#'+row).append("<a href='#' name='editBtn' data-val='"+data[row]['id']+"' data-name='"+data[row]['name']+"' data-url='"+data[row]['url']+"' title='edit'><i class='icon-edit icon-gray'></i></a>&nbsp;");
+
+		$('#'+row).append("<a href='#' name='deleteBtn' data-val='"+data[row]['id']+"' title='delete'><i class='icon-remove icon-gray'></i></a>");
+	}
+    //handle delete click
+    $("a[name='deleteBtn']").click(function(e){
+	e.preventDefault();
+	var val = $(e.currentTarget).data('val');
+	var data = {id:val};
+	$('#delSite').data('val',val);
+	//show delete modal
+	$('#deleteModal').modal('show');
+    });
+    //handle edit click
+    $("a[name='editBtn']").click(function(e){
+	e.preventDefault();
+	var val = $(e.currentTarget).data('val');
+	var name = $(e.currentTarget).data('name');
+	var url = $(e.currentTarget).data('url');
+	data = {id:val,url:url,name:name};
+	$('#formID').val(val);
+	$('#myModal').modal('show');
+	//change label to edit
+	$('#myModalLabel').html('Edit :' + name);
+	$('#siteName').val(name);
+	$('#siteUrl').val(url);
+	$('#editSite').show();
+	$('#addSite').hide();
+
+
+
+    });
+
+
+
+}
---
+++ dbconn.php
@@ -0,0 +1,89 @@
+<?php
+
+include_once('myClass.php');
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "yogidog88";
+$query = "";
+$queryCheck = $_POST['query'];
+$queryData = $_POST['data'];
+
+$querycheck = str_replace("\'","'",$querycheck );
+
+$conn = mysql_connect($dbhost, $dbuser, $dbpass) or die('Error connecting to mysql');
+
+$dbname = 'DB_TOM';
+mysql_select_db($dbname)or die ("could not select db: ".mysql_error()."OH SNAP");
+
+switch ($queryCheck) {
+    case 'getSites':
+        $query = "select name,url,id from sites where del=0;";
+        break;
+    case 'getCustomers':
+        $query = "select * from customers;";
+        break;
+    case 'getCustomersBySite':
+         //queryData;
+        $query = "select * from sites s, customers c where s.id = c.websiteid;";
+         break;
+    case 'addSite':
+         include "tool.php";
+        $query = "INSERT INTO sites (name,url,id,del)".
+                   " VALUES ('".$queryData[name]."','".$queryData[url]."','".$guid."',0); ";
+        break;
+    case 'updateSite':
+        $query = "UPDATE sites SET name = '".$queryData[name]."',url = '".$queryData[url]."',del = 0 WHERE id = '".$queryData[id]."';";
+        break;
+
+case 'addCustomer':
+         include "tool.php";
+        $query = "INSERT INTO customers (first,last,address,phone,email,sourceip,id,sitesid)".
+                   " VALUES (".$queryData[first].",".$queryData[last].",".$queryData[address].",".$queryData[phone].",".$queryData[email].",".$queryData[sourceip].",".$guid.",".$queryData[sitesid]."); ";
+        break;
+    case 'delSite':
+        $query = "UPDATE sites SET del = '1' WHERE id = '".$queryData[id]."';";
+        break;
+    case 'delCustomer':
+        $query = "UPDATE customer SET del = '1' WHERE id = '".$queryData[id]."';";
+        break;
+
+}
+
+if($queryCheck == 'getSites' || $queryCheck == 'getCustomers'){
+        $result = mysql_query($query) or die ("Query failed: " . mysql_error() . " Actual query: " . $query);
+        $rows = array();
+        while($r = mysql_fetch_assoc($result)){
+                $rows[]=$r;
+        }
+        $resp = new stdClass();
+        $resp->success = false;
+        if($result) {
+                $resp->success = true;
+                $resp->type = $queryCheck;
+                $resp->results = $rows;
+        }
+        print json_encode($resp);
+
+}else if($queryCheck == 'addSite' || $queryCheck == 'updateSite'){
+        $result = mysql_query($query, $conn);
+        $resp = new stdClass();
+        $resp->success = false;
+        if($result) {
+                $resp->type = $queryCheck;
+                $resp->success = true;
+        }
+        print json_encode($resp);
+}else if($queryCheck == 'delSite'){
+        $result = mysql_query($query, $conn);
+        $resp = new stdClass();
+        $resp->success = false;
+        if($result) {
+                $resp->type = $queryCheck;
+                $resp->success = true;
+        }
+        print json_encode($resp);
+}
+
+?>
+
